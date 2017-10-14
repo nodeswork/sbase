9e945acd5074ec68f5930139b5eefb87c30d8eec
Yizhen Zhao <yizhen@uber.com>
2017-10-13 23:16:46 -0700

    Filter empty metrics models.

--- src/metrics/metrics-models.ts
+++ src/metrics/metrics-models.ts
@@ -144,6 +144,7 @@ export class MetricsModel extends sMongoose.Model implements m.MetricsData {
 
     return _
       .chain(metricsModels)
+      .filter((model) => model.dimensions == null || model.metrics == null)
       .groupBy((model) => {
         return Math.floor(model.ts.getTime() / options.granularityInSecond);
       })
